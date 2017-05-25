a8436e122d34589a972d98663e8a0fce4a888a85
Ben Pfaff <blp@ovn.org>
2017-05-25 14:45:02 -0700

    Support accepting and displaying port names in OVS tools.
    Until now, most ovs-ofctl commands have not accepted names for ports, only
    numbers, and have not been able to display port names either.  It's a lot
    easier for users if they can use and see meaningful names instead of
    arbitrary numbers.  This commit adds that support.
    
    For backward compatibility, only interactive ovs-ofctl commands by default
    display port names; to display them in scripts, use the new --names
    option.
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Tested-by: Aaron Conole <aconole@redhat.com>

--- ofproto/ofproto-dpif.c
+++ ofproto/ofproto-dpif.c
@@ -4977,7 +4977,7 @@ ofproto_unixctl_fdb_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
         char name[OFP10_MAX_PORT_NAME_LEN];
 
         ofputil_port_to_string(ofbundle_get_a_port(bundle)->up.ofp_port,
-                               name, sizeof name);
+                               NULL, name, sizeof name);
         ds_put_format(&ds, "%5s  %4d  "ETH_ADDR_FMT"  %3d\n",
                       name, e->vlan, ETH_ADDR_ARGS(e->mac),
                       mac_entry_age(ofproto->ml, e));
@@ -5019,7 +5019,7 @@ ofproto_unixctl_mcast_snooping_show(struct unixctl_conn *conn,
 
             bundle = b->port;
             ofputil_port_to_string(ofbundle_get_a_port(bundle)->up.ofp_port,
-                                   name, sizeof name);
+                                   NULL, name, sizeof name);
             ds_put_format(&ds, "%5s  %4d  ", name, grp->vlan);
             ipv6_format_mapped(&grp->addr, &ds);
             ds_put_format(&ds, "         %3d\n",
@@ -5033,7 +5033,7 @@ ofproto_unixctl_mcast_snooping_show(struct unixctl_conn *conn,
 
         bundle = mrouter->port;
         ofputil_port_to_string(ofbundle_get_a_port(bundle)->up.ofp_port,
-                               name, sizeof name);
+                               NULL, name, sizeof name);
         ds_put_format(&ds, "%5s  %4d  querier             %3d\n",
                       name, mrouter->vlan,
                       mcast_mrouter_age(ofproto->ms, mrouter));
